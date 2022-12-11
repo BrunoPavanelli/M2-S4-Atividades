fb907c9eefb5adb8bf1819fc1470109862a20009
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-12-11 20:08:54 -0300

    starting lesson: generating students list

---
+++ Gerando lista de alunos/script.js
@@ -0,0 +1,22 @@
+const alunos = [
+    {
+     nome: "Felipe",
+     nota: 9.2
+    },
+    {
+     nome: "João",
+     nota: 5.5
+    },
+    {
+     nome: "Julia",
+     nota: 8.2
+    },
+    {
+     nome: "Maria",
+     nota: 4.9
+    },
+    {
+     nome: "Marcos",
+     nota: 6
+    },
+ ]
