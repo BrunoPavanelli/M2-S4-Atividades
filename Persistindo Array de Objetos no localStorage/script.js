5fccb1b1ac8f6c1ba3df05f7e94f57770f35b814
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-12-12 17:04:35 -0300

    in lesson: persist object array in localStorage: add and remove tasks functionalities implementeds, now start creating localStorage functionalities

--- Persistindo Array de Objetos no localStorage/script.js
+++ Persistindo Array de Objetos no localStorage/script.js
@@ -1,1 +1,48 @@
-// Desenvolva sua lógica aqui
+function createTask() {
+    const tasksList = document.querySelector('.tasks__list')
+    const taskCreatorInput = document.querySelector('.create-tasks__inpt')
+
+    const listBox = document.createElement('li')
+    const listTask = document.createElement('p')
+    const btnTaskRemove = document.createElement('button')
+
+    listBox.classList.add('tasks__item', 'container', 'intern-padding', 'space-between', 'item-height', 'border-radius__8')
+    btnTaskRemove.classList.add('tasks__remove')
+    btnTaskRemove.dataset.id = tasksList.childElementCount + 1
+
+    listTask.innerText = taskCreatorInput.value
+    btnTaskRemove.innerText = 'Remove'
+
+    listBox.append(listTask, btnTaskRemove)
+
+    return listBox
+}
+
+function addTasksToList() {
+    const tasksList = document.querySelector('.tasks__list')
+    const btnAddTasksToList = document.querySelector('.create-task__submit')
+
+    btnAddTasksToList.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        const task = createTask()
+
+        tasksList.append(task)
+
+        removeTaskFromList()
+    })
+}
+
+function removeTaskFromList() {
+    const btnRemoveTasksFromList = document.querySelectorAll('.tasks__remove')
+
+    btnRemoveTasksFromList.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const listBox = e.composedPath()[1]
+            
+            listBox.remove()
+        })
+    })
+}
+
+addTasksToList()
