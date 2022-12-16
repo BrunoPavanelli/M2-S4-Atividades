96a0b20b32e94fd303df5458730196f00ee73d17
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-12-16 20:15:52 -0300

    started lesson: creating own method in prototype JS

---
+++ Criando o Próprio Método no Prototype JS/database.js
@@ -0,0 +1,26 @@
+// Banco de dados
+
+const dataBase = [
+    1,
+    25,
+    "Samu",
+    true,
+    854,
+    6587,
+    false,
+    "",
+    null,
+    "Apollo",
+    "Silas",
+    {},
+    0,
+    467,
+  ];
+
+  Array.prototype.findNumberEven = function (array) {
+    for (let i = 0; i < this.length; i++) {
+            array(this[i]) 
+     }
+  }
+
+  dataBase.findNumberEven()
